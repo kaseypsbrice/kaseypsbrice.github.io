17a57adc5d7eb1a6024d900a8657bec64cf75291
Kasey Brice <kbrice@student.42adel.org.au>
2024-05-12 13:03:18 +0930

    Basic vue setup

---
+++ src/components/NavBar.vue
@@ -0,0 +1,23 @@
+<template>
+	<header>
+		<nav>
+			<div> <!-- Title of website -->
+				<h1>KB</h1>
+			</div>
+			<ul class="routes"> <!-- Navigation links -->
+				<RouterLink to="/">Home</RouterLink>
+				<RouterLink to="/projects">Projects</RouterLink>
+				<RouterLink to="/resume">Resume</RouterLink>
+				<RouterLink to="/contact">Contact</RouterLink>
+			</ul>
+		</nav>
+	</header>
+</template>
+
+<script setup>
+import { RouterLink } from 'vue-router'
+</script>
+
+<style>
+
+</style>
